6a9c0b7507276960056a1e33d1bd0ff895e25540
Jennieouni <jennie66688@gmail.com>
2020-09-22 16:11:13 +0800

    3

--- src/components/finish.vue
+++ src/components/finish.vue
@@ -200,6 +200,7 @@ export default {
     }
   },
   mounted () {
+    console.log(111)
     axios.get('/ScoreAppointTag/0').then(response => {
       this.scoreOver = response.data[2]
       // console.log(this.scoreOver)
